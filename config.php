248fd1ecb3a444d7a596bd09456e65b5f69a68fe
killernoob2 <45244658+killernoob2@users.noreply.github.com>
2019-04-02 19:31:07 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="killernoob ";
+
+//user_gaid wajib isi
+$user_gaid= "b0404b23-a17c-41e3-97f4-904bac3edb61";
+//user_token wajib di isi //
+$user_token="AbzQQD2nsHGGm4aL";
+
+//user_id wajib di isi //
+$user_id="Facebook_785856405083862";
+
+// invite_code wajib di isi //
+$invite_code="F3b9RRl7sE";
+
+// Bearer wajib di isi //
+$Bearer="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va18zMzUzMTg5ODM5OTQwODQiLCJleHAiOiIxNTU1MDAzNDIyMTc1In0.RcT6lA6AgYWsrtTzQJOYj1QnMyAFl27uBwkQORh-x_s";
+
+
+
+?>
